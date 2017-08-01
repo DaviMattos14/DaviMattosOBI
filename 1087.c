c65e905ae99449bce4d7b01c7e35d47cf817c9a3
Davi Mattos <davimattos2012@gmail.com>
2017-08-01 17:51:13 -0300

    Update 1087.c

--- 1087.c
+++ 1087.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
-#include <string.h>
-int main(void)
+int xadrez[8][8];
+int resposta[]
+int olharMatriz(int x1, int y1, int x2, int y2)
 {
-	int x1, y1, x2, y2, i, j, count = 0;
-	int xadrez[8][8];
-
 	for (i = 0; i < 8; i++)
-		for(j = 0; j < 8; j++)
-		{
-			xadrez[i][j] = 0;
-			count++;
-		}
-	printf("%d\n", count);
-	do{scanf("%d %d %d %d", &x1, &y1, &x2, &y2);}
+	for(j = 0; j < 8; j++){xadrez[i][j] = 0;}
+	while(x1 == 8 || y1 == 8)
+	{
+		x1++;
+		y1++;
+		
+	}
+}
+
+int main(void) 
+{
+	int x1, y1, x2, y2, i, j;
+	int xadrez[8][8];
+	do{
+		scanf("%d %d %d %d", &x1, &y1, &x2, &y2); 
+		olharMatriz(x1, y1, x2, y2);
+	}
 	while(x1 != 0 && y1 != 0 && x2 != 0 && y2 != 0);
-
-
 	return 0;
 }
